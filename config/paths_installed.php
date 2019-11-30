61e06c631219d7b3131ab79b8d524542483a7954
precious <tobiprecious13@gmail.com>
2019-11-30 06:29:19 +0200

    first commit

---
+++ config/paths_installed.php
@@ -0,0 +1,5 @@
+<?php
+  define('PATH_DATA',         '/opt/processmaker/shared/');
+  define('PATH_C',            '/opt/processmaker/shared/compiled/');
+  define('HASH_INSTALLATION', 'z6PGlM2ZoKvZZMhsxmhompZjlm7Ga5qWkmpna5JnnGjEa5lpkmRqbMii1KjZa5qVkmRibcZkyG3Gbm2YYmZqacRoxmfCaGdulmOebciHhLRhY6WUlGaU');
+  define('SYSTEM_HASH',       '4c3a118e0e9e77b1315a4c4a7661394c');
